56490a950e5fd13358da58133c1df93c40aa18f4
muradmaharramly <mooradmaharramly@gmail.com>
2026-04-26 10:55:40 +0400

    sass global injection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "${path.resolve(__dirname, 'src/styles/variables.scss').replace(/\\/g, '/')}";
+          @import "${path.resolve(__dirname, 'src/styles/modals.scss').replace(/\\/g, '/')}";
+        `
+      }
+    }
   }
 })
