8fd66cd0c9a224603daea4c33072a33d867c27c7
muradmaharramly <mooradmaharramly@gmail.com>
2026-04-26 10:59:35 +0400

    deploy edit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -9,17 +7,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
   build: {
     chunkSizeWarningLimit: 2000,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "${path.resolve(__dirname, 'src/styles/variables.scss').replace(/\\/g, '/')}";
-          @import "${path.resolve(__dirname, 'src/styles/modals.scss').replace(/\\/g, '/')}";
-        `
-      }
-    }
   }
 })
